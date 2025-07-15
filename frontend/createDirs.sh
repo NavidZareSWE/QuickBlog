edd9ea8a16bd2c74bb3e26dd83bca8360a2dcb9b
navidzare7775 <navidzare7775@gmail.com>
2025-07-15 14:35:46 +0330

    Initial Commit

---
+++ frontend/createDirs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Base directory
+BASE_DIR="src"
+
+# Create directories
+mkdir -p "$BASE_DIR/assets/fonts"
+mkdir -p "$BASE_DIR/assets/images"
+mkdir -p "$BASE_DIR/components/common"
+mkdir -p "$BASE_DIR/components/module"
+mkdir -p "$BASE_DIR/components/templates"
+mkdir -p "$BASE_DIR/config"
+mkdir -p "$BASE_DIR/constants"
+mkdir -p "$BASE_DIR/contexts"
+mkdir -p "$BASE_DIR/hooks"
+mkdir -p "$BASE_DIR/lib"
+mkdir -p "$BASE_DIR/pages"
+mkdir -p "$BASE_DIR/services"
+mkdir -p "$BASE_DIR/styles"
+mkdir -p "$BASE_DIR/utils/api"
+
+echo "Directories created successfully."
