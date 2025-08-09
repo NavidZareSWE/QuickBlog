0f1d85efe68a52362d82dc9fe91809dbcdc98c31
navidzare7775 <navidzare7775@gmail.com>
2025-08-09 16:34:12 +0330

    Working_Branch_1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/module/Navbar";
 import Header from "../components/templates/Header";
 import BlogList from "../components/templates/BlogList";
 import Newsletter from "../components/module/Newsletter";
-import Footer from "../components/module/footer";
+import Footer from "../components/module/Footer";
 
 const Home = () => {
   return (
